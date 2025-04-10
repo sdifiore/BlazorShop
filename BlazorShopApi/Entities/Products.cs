a02c761bd217a05bbd444ff074e8da391da20ad8
Sergio Di Fiore <difiores@outlook.com>
2025-04-10 15:51:19 -0300

    Add project files.

---
+++ BlazorShopApi/Entities/Products.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorShopApi.Entities
+{
+	public class Products
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public string ImageUrl { get; set; } = string.Empty;
+		public decimal Price { get; set; }
+		public int Quantity { get; set; }
+	}
+}
