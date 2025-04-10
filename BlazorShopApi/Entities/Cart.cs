7ca8eac41c58886345100f9357d86e0bef64a73a
Sergio Di Fiore <difiores@outlook.com>
2025-04-10 15:58:12 -0300

    Entities.Entities basic definition

---
+++ BlazorShopApi/Entities/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorShopApi.Entities
+{
+	public class Cart
+	{
+		public int Id { get; set; }
+		public string UserId { get; set; } = string.Empty;
+	}
+}
---
+++ BlazorShopApi/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShopApi.Entities
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string IconCSS { get; set; } = string.Empty;
+	}
+}
